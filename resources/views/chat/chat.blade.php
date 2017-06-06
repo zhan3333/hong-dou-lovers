c56d715b8fda0ca5826f26f01a76e700face35dd
zhan <grianchan@gmail.com>
2017-06-06 09:24:22 +0800

    修改样式

--- resources/views/chat/chat.blade.php
+++ resources/views/chat/chat.blade.php
@@ -6,11 +6,17 @@
             <div class="col-md-8 col-md-offset-2">
                 <form class="form-horizontal">
                     <div class="form-group">
-                        <label for="inputType" class="col-sm-2">Type</label>
+                        <label for="inputType" class="col-sm-2">Type </label>
                         <div class="col-sm-10">
-                            <input type="text" id="inputType" class="form-control" placeholder="Type">
+                            <label class="radio-inline">
+                                <input type="radio" name="inputType" id="inputType" value="1" checked> 单聊
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="inputType" id="inputType" value="2"> 群聊
+                            </label>
                         </div>
                     </div>
+
                     <div class="form-group">
                         <label for="inputTo" class="col-sm-2">To </label>
                         <div class="col-sm-10">
@@ -20,10 +26,16 @@
                     <div class="form-group">
                         <label for="inputContent" class="col-sm-2">Content</label>
                         <div class="col-sm-10">
-                            <input type="text" id="inputContent" class="form-control" placeholder="Content">
+                            <div class="row">
+                                <div class="col-sm-8 col-xs-8">
+                                    <input type="text" id="inputContent" class="form-control" placeholder="Content">
+                                </div>
+                                <div class="col-sm-4 col-xs-4">
+                                    <button id="sendMessage" type="button" class="btn btn-default">Submit</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <button id="sendMessage" type="button" class="btn btn-default">Submit</button>
                 </form>
             </div>
         </div>
